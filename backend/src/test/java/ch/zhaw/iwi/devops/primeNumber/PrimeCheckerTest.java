7e92c09cede4a6af32cf6fbee10b5a6868d9cf1c
mansi2019 <singhma3@students.zhaw.ch>
2021-05-22 17:36:07 +0200

    added unit tests for primeChecker

--- backend/src/test/java/ch/zhaw/iwi/devops/primeNumber/PrimeCheckerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/primeNumber/PrimeCheckerTest.java
@@ -1,5 +1,7 @@
 package ch.zhaw.iwi.devops.primeNumber;
 
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,4 +14,19 @@ public class PrimeCheckerTest {
         Assert.assertEquals(false, primeChecker.checker(1));
 
     }
+    @Test
+    public void PrimeChecker3() {
+
+        PrimeChecker primeChecker = new PrimeChecker();
+        Assert.assertEquals(true, primeChecker.checker(3));
+    }
+
+    @Test
+    public void PrimeCheckerRandom() {
+
+        PrimeChecker primeChecker = new PrimeChecker();
+        int random_int = (int)Math.floor(Math.random()*2);  //jede Zahl multipliziert mit 2 sollte keine Primzahl ergeben
+        Assert.assertEquals(false, primeChecker.checker(random_int));
+    }
+    
 }
